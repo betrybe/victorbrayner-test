58a99d766195848b9b154e5f52b8c3881aaf8f61
Victor Brayner <victorbrayner@gmail.com>
2021-10-27 22:58:36 -0300

    script aprovado no linter

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let items = null;
+let cart = null;
+let allProducts = null;
+let emptyCart = null;
+let cartSaved = [];
+let cartPrice = 0;
+let totalPrice = null;
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -13,32 +21,33 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+function totalPriceOfItems(price) {
+  cartPrice += price;
+  totalPrice.innerText = `${cartPrice}`;
 }
 
 function cartItemClickListener(event) {
   cartSaved.forEach((item) => {
-    if (item.sku === event.target.innerText.split(' ')[1]){
-      const index = cartSaved.indexOf(item)
-      cartSaved.splice(index, 1)
-      localStorage.setItem('cart', JSON.stringify(cartSaved))
+    if (item.sku === event.target.innerText.split(' ')[1]) {
+      const index = cartSaved.indexOf(item);
+      cartSaved.splice(index, 1);
+      localStorage.setItem('cart', JSON.stringify(cartSaved));
     }
-  })
-  cart.removeChild(event.target)
-  totalPriceOfItems(-(event.target.innerText.split('$')[1]))
+  });
+  
+  cart.removeChild(event.target);
+  totalPriceOfItems(-(event.target.innerText.split('$')[1]));
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -49,95 +58,83 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-async function fetchCountries(){
-  const keyword = 'computador'
-  items.innerHTML = `<div class="loading">Carregando itens disponíveis...</div>`
-  const res = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${keyword}`);
-  const json = await res.json();
-  items.innerHTML = ``
-  
-  allProducts = Array.from(json.results)  
-  
-  totalPrice.innerText = 0
-
-  if (localStorage.getItem('cart')) {
-    const localProduct = JSON.parse(localStorage.getItem('cart'))
-    localProduct.forEach((product) => {
-      cart.appendChild(createCartItemElement(product));
-      totalPriceOfItems(product.salePrice)
-    })
-  }
-
-   
-  render();
-}
-
-function render(){
-  allProducts.forEach((product) => {
-    let obj = {
-      sku: product.id,
-      name: product.title,
-      image: product.thumbnail
-    }
-    items.appendChild(createProductItemElement(obj));
-
-  })
-  handleButtons()
-}
-
-function handleButtons(){
-  const cartBtn = Array.from(items.querySelectorAll('.item__add'));
-  
-  cartBtn.forEach((button, index) => {
-    button.addEventListener('click',() => productToCart(index));
-  })
-  
-  emptyCart.addEventListener('click', removeItemsFromCart)
-  
-  function removeItemsFromCart(){
-    const cItemsArr = Array.from(cart.querySelectorAll('.cart__item'))
-    localStorage.removeItem('cart')
-    cItemsArr.forEach((item) => {
-      cart.removeChild(item)
-    })
-    totalPrice.innerText = 0
-    cartPrice = 0
-  }
-}
-
-async function productToCart(index){
-  const itemID = allProducts[index].id
+async function productToCart(index) {
+  const itemID = allProducts[index].id;
   const res = await fetch(`https://api.mercadolibre.com/items/${itemID}`);
   const json = await res.json();
   
-  let obj = {
+  const obj = {
     sku: json.id,
     name: json.title,
-    salePrice: json.price
-  }
+    salePrice: json.price,
+  };
+
   cart.appendChild(createCartItemElement(obj));
-  cartSaved = [...cartSaved, obj]
-  localStorage.setItem('cart', JSON.stringify(cartSaved))
-  totalPriceOfItems(obj.salePrice)
+  cartSaved = [...cartSaved, obj];
+  localStorage.setItem('cart', JSON.stringify(cartSaved));
+  totalPriceOfItems(obj.salePrice);
 } 
 
-function totalPriceOfItems(price){
-  cartPrice += price
-  totalPrice.innerText = `${cartPrice}`
+function handleButtons() {
+  const cartBtn = Array.from(items.querySelectorAll('.item__add'));
+
+  cartBtn.forEach((button, index) => {
+    button.addEventListener('click', () => productToCart(index));
+  });
+  
+  function removeItemsFromCart() {
+    const cItemsArr = Array.from(cart.querySelectorAll('.cart__item'));
+    localStorage.removeItem('cart');
+    cItemsArr.forEach((item) => {
+      cart.removeChild(item);
+    });
+    totalPrice.innerText = 0;
+    cartPrice = 0;
+  }
+
+  emptyCart.addEventListener('click', removeItemsFromCart);
 }
 
-let items = null
-let cart = null
-let allProducts = null
-let emptyCart = null
-let cartSaved = []
-let cartPrice = 0
-let totalPrice = null
+function render() {
+  allProducts.forEach((product) => {
+    const obj = {
+      sku: product.id,
+      name: product.title,
+      image: product.thumbnail,
+    };
+
+    items.appendChild(createProductItemElement(obj));
+  });
+
+  handleButtons();
+}
+
+async function loadProducts() {
+  const keyword = 'computador';
+  items.innerHTML = '<div class="loading">Carregando itens disponíveis...</div>';
+  const res = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${keyword}`);
+  const json = await res.json();
+  items.innerHTML = '';
+
+  allProducts = Array.from(json.results);
+  totalPrice.innerText = 0;
+
+  if (localStorage.getItem('cart')) {
+    const localProduct = JSON.parse(localStorage.getItem('cart'));
+    localProduct.forEach((product) => {
+      cart.appendChild(createCartItemElement(product));
+      totalPriceOfItems(product.salePrice);
+    });
+  }
+
+  render();
+}
 
 window.onload = () => {
-  items = document.querySelector(".items")
-  cart = document.querySelector(".cart__items")
-  emptyCart = document.querySelector(".empty-cart")
-  totalPrice = document.querySelector(".total-price")
-  fetchCountries()
- };
+  items = document.querySelector('.items');
+  cart = document.querySelector('.cart__items');
+  emptyCart = document.querySelector('.empty-cart');
+  totalPrice = document.querySelector('.total-price');
+
+  loadProducts();
+};
